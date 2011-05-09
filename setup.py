21025f2833628933d56e1b1797429fbe0f29cfea
Boaz Leskes <boaz@buzzcapture.com>
2011-05-09 20:51:02 +0200

    Eearly pypi name clame.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 import sys, os
 
-version = '1.0dev'
+version = '0.0dev'
 
 long_description = (
     open('README.txt').read()
@@ -10,18 +10,18 @@ long_description = (
 
 setup(name='pycounters',
       version=version,
-      description='A Python based light weight manual instrumantion livrary',
+      description='A Python based light weight manual instrumantion livrary. Currenty just a place holder. More good things to come.',
       long_description=long_description,
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='',
       author='PyCounters Developers',
       author_email='b.leskes@gmail.com',
       url='',
-      license='apache',
+      license='apache2',
       packages=['pycounters'],
       package_dir = {'': 'src'},
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-      	'typeinfo',
+      	
       ])
