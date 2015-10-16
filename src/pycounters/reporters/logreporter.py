d26810942d0e655e6c10bdb54e60b19de1ffb0e5
Huu Nguyen <huu@prismskylabs.com>
2015-10-16 14:08:32 -0400

    Move logreporter into its own module

---
+++ src/pycounters/reporters/logreporter.py
@@ -0,0 +1,21 @@
+from . import BaseReporter
+
+
+class LogReporter(BaseReporter):
+    """ Log based reporter.
+    """
+
+    def __init__(self, output_log=None):
+        """ output will be logged to output_log
+            :param output_log: a python log object to output reports to.
+        """
+        super(LogReporter, self).__init__()
+        self.logger = output_log
+
+    def output_values(self, counter_values):
+        logs = sorted(counter_values.iteritems(), cmp=lambda a, b: cmp(a[0], b[0]))
+
+        for k, v in logs:
+            if not (k.startswith("__") and k.endswith("__")):   # don't output __node_reports__ etc.
+                self.logger.info("%s %s", k, v)
+
